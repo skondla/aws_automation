396dce850a64f27500e6d43ee1927e9410ddeae0
skondla <7891645+skondla@users.noreply.github.com>
2023-04-30 21:20:35 -0400

    updated findStringFromMultpleFileTypes.sh..

--- findStringFromMultpleFileTypes.sh
+++ findStringFromMultpleFileTypes.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+#Author:skondla@me.com
+#Purpose: Find set of strings from multiple type files from all sub-directories
+
 #find . -name "*.md" -o -name "*.py" -o -name "*.sh" -o -name "*.sh" \
 # |xargs egrep -i "(yahoo.com|me.com|gmail.com)"
 
