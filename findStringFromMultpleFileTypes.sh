eeb78fc45a72abbfe9427bf5ded242334fcbc0f0
skondla <kondlawork@gmail.com>
2025-01-05 20:31:57 -0500

    updated

--- findStringFromMultpleFileTypes.sh
+++ findStringFromMultpleFileTypes.sh
@@ -5,4 +5,4 @@
 #find . -name "*.md" -o -name "*.py" -o -name "*.sh" -o -name "*.sh" \
 # |xargs egrep -i "(yahoo.com|me.com|gmail.com)"
 
-find -E . -regex '.*\.(py|sh|sql|md)' |xargs egrep -i "(yahoo.com|me.com|gmail.com)"
+find -E . -regex '.*\.(py|sh|sql|md)' |xargs egrep -i "(yahoo.com|me.com|gmail.com|outlook.com)"
